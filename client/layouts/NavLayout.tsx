66fb85490801951e92f94bee112dd2bbaaf81e74
Andres Aguirre <aguirregzz97@gmail.com>
2022-05-03 13:04:00 -0500

    remove nav layout

--- client/layouts/NavLayout.tsx
+++
@@ -1,8 +0,0 @@
-import { Box } from "@chakra-ui/layout"
-import React from "react"
-
-const NavLayout: React.FC = ({ children }) => {
-  return <Box ml={{ base: "none", sm: "160px" }}>{children}</Box>
-}
-
-export default NavLayout
